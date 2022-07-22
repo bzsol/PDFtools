80bbd4b7f6fb1c7de00010194707d8371adab2c4
bzsol <bereczzsolt2000@gmail.com>
2022-07-22 23:15:53 +0200

    Added maven project

--- toolsPDF/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.pdftools.toolspdf {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.pdftools.toolspdf to javafx.fxml;
-    exports com.pdftools.toolspdf;
-}
